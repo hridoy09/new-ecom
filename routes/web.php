84cccb4e69132cda54c310f325550381aaf98e12
hridoy09 <mdabulhasemredoy@gmail.com>
2023-02-06 14:59:05 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Http\Controllers\admin\CategoryController;
+use App\Http\Controllers\admin\SubcategoryController;
+use App\Http\Controllers\admin\ProductController;
+use App\Http\Controllers\admin\SliderController;
+use App\Http\Controllers\admin\StockController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\frontend\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('category', CategoryController::class);
+Route::resource('subcategory', SubcategoryController::class);
+Route::resource('product', ProductController::class);
+Route::resource('stock', StockController::class);
+Route::resource('slider', SliderController::class);
+
+
+Route::name('admin.')->prefix('admin/')->group(function(){
+Route::get('dashboard', [AdminController::class, 'index']);
+
+});
+
+Route::name('user.')->prefix('user/')->group(function(){
+    Route::get('home', [HomeController::class, 'index']);
+    Route::get('shop/{id}',[HomeController::class, 'shop'])->name('shop');
+
+});
+
+
